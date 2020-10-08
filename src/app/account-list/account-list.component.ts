5713c8dc2526509e7c882ed5b196823a8e130a3d
Trey Edwards <trey.x.edwards@gmail.com>
2020-10-08 15:11:35 -0400

    Initial Commit
    Initial Commit

---
+++ src/app/account-list/account-list.component.ts
@@ -0,0 +1,55 @@
+import {Component, OnInit} from '@angular/core';
+import {AccountServiceService} from './account-service.service';
+import { AccountElement} from './account-service.service';
+
+const INITIAL_BLOCK_SIZE = 3;
+
+const ACCOUNT = 'account';
+const BALANCE = 'balance';
+const ASCENDING = 'asc';
+const DESCENDING = 'desc';
+
+@Component({
+  selector: 'app-account-list',
+  templateUrl: './account-list.component.html',
+  styleUrls: ['./account-list.component.css']
+})
+
+export class AccountListComponent implements OnInit {
+
+  dataSource: AccountElement[];
+  fetchComplete = false;
+
+  accountSortDirection = ASCENDING;
+  balanceSortDirection = ASCENDING;
+
+  constructor(private accountService: AccountServiceService) { }
+
+  ngOnInit(): void {
+    this.dataSource = this.accountService.fetchData(INITIAL_BLOCK_SIZE);
+  }
+
+  onLoadMore(): void {
+    this.dataSource = this.accountService.loadData();
+    this.fetchComplete = true;
+  }
+
+  onSortChange(columnName: string): void {
+    if (!!columnName) {
+      switch (columnName) {
+        case ACCOUNT:
+          this.accountSortDirection = this.accountSortDirection === ASCENDING ? DESCENDING : ASCENDING;
+          const accountSortObject = { sortColumn: 'account', sortDirection: this.accountSortDirection};
+          const accountBlockSize = !this.fetchComplete ? INITIAL_BLOCK_SIZE : undefined;
+          this.dataSource = this.accountService.fetchData(accountBlockSize, accountSortObject);
+          break;
+        case BALANCE:
+          this.balanceSortDirection = this.balanceSortDirection === ASCENDING ? DESCENDING : ASCENDING;
+          const balanceSortObject = { sortColumn: 'balance', sortDirection: this.balanceSortDirection};
+          const balanceBlockSize = !this.fetchComplete ? INITIAL_BLOCK_SIZE : undefined;
+          this.dataSource = this.accountService.fetchData(balanceBlockSize, balanceSortObject);
+          break;
+      }
+    }
+  }
+}
---
+++ src/app/account-list/account-service.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+
+
+export interface AccountElement {
+  name: string;
+  balance: number;
+  changeAmount: number;
+  pctChange: number;
+}
+
+const ACCOUNT = 'account';
+const BALANCE = 'balance';
+const ASCENDING = 'asc';
+const DESCENDING = 'desc';
+
+const ACCOUNT_DATA: AccountElement[] = [
+  {name: 'IRA - 5200', balance: 5763.36, changeAmount: 8916.69, pctChange: -0.08},
+  {name: 'AAA - 1812', balance: 2010926.10, changeAmount: 38881.63, pctChange: 0.21},
+  {name: 'AAA - 3810', balance: 10050054.07, changeAmount: 8916.69, pctChange: 0.07},
+  {name: 'REG - 2019', balance: 13465679.34, changeAmount: 0.00, pctChange: 0.00},
+  {name: 'IRA - 0146', balance: 15884302.39, changeAmount: 7430.83, pctChange: 0.03},
+  {name: 'AAA - 0029', balance: 39160334.42, changeAmount: 31435.87, pctChange: -0.07}
+];
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AccountServiceService {
+
+  constructor() { }
+
+  loadData(blockSize?: number): AccountElement[] {
+    if (blockSize > 0 && blockSize <= ACCOUNT_DATA.length) {
+      return ACCOUNT_DATA.slice(0, blockSize);
+    } else {
+      return ACCOUNT_DATA.slice();
+    }
+  }
+
+  fetchData(blockSize?: number, sortObject?: any): AccountElement[] {
+    const sortColumn = sortObject && !!sortObject.sortColumn ? sortObject.sortColumn : undefined;
+    const sortDirection = sortObject && !!sortObject.sortDirection ? sortObject.sortDirection : undefined;
+
+    if (!!sortColumn && !!sortDirection) {
+      switch (sortColumn) {
+        case ACCOUNT:
+          const sortedNameData = ACCOUNT_DATA.sort((a, b) => {
+                const name1: number = +a.name.substr(5, 4);
+                const name2: number = +b.name.substr(5, 4);
+                if (sortDirection === ASCENDING) {
+              return name1 - name2;
+            } else if (sortDirection === DESCENDING) {
+              return name2 - name1;
+            }
+          });
+          return !!blockSize ? sortedNameData.slice(0, blockSize) : sortedNameData.slice();
+        case BALANCE:
+          const sortedBalanceData = ACCOUNT_DATA.sort((a, b) =>
+                sortDirection === ASCENDING ? a.balance - b.balance : b.balance - a.balance);
+          return !!blockSize ? sortedBalanceData.slice(0, blockSize) : sortedBalanceData.slice();
+      }
+    } else {
+      return !!blockSize ? ACCOUNT_DATA.slice(0, blockSize) : ACCOUNT_DATA.slice();
+    }
+  }
+}
